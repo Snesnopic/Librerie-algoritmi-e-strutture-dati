f21213afe1568af20b3240329d91dfacd450bd4b
Giuseppe <snesnopic@gmail.com>
2022-03-27 17:51:50 +0200

    Update container.cpp

--- container/container.cpp
+++ container/container.cpp
@@ -8,27 +8,37 @@ namespace lasd
 //Container functions
 bool Container::Empty()const noexcept
 {
-    if(size == 0)
-        return true;
-    return false;
+    return (size == 0);
 }
 unsigned long Container::Size()const noexcept
 {
     return size;
 }
-
+void Container::Clear()
+{
+    this->size = 0;
+}
 //LinearContainer functions
 template<typename Data>
 Data LinearContainer<Data>::Front() const
 {
+    if(this.size == 0)
+        throw std::out_of_range("Index out of bounds!");
     return this[0];
 }
 template<typename Data>
 Data LinearContainer<Data>::Back() const
 {
+    if(this.size == 0)
+        throw std::out_of_range("Index out of bounds!");
     return this[size-1];
 }
-
+template <typename Data>
+bool FoldableContainer<Data>::Exists(const Data d)
+{
+    //chiamata a FoldableContainer::Fold()
+    return true;
+}
 
 
 /* ************************************************************************** */
