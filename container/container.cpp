45a1c32f66b31ea5333f66e35f3771f803d80afb
Giuseppe <snesnopic@gmail.com>
2022-03-30 16:01:47 +0200

    Update container.cpp

--- container/container.cpp
+++ container/container.cpp
@@ -14,51 +14,104 @@ unsigned long Container::Size()const noexcept
 {
     return size;
 }
-void Container::Clear()
-{
-    this->size = 0;
-}
-//LinearContainer functions
 
-bool LinearContainer<Data>::operator==(const Vector &v)
+//LinearContainer functions
+bool LinearContainer<Data>::operator==(const LinearContainer<Data> &lc) const noexcept
 {
     if(this.size != v.Size())
         return false;
-    for(int i=0 ; i<this.size; i++)
+    for(unsigned long i=0 ; i<this.size; i++)
     {
         if(operator[](i) != v[i])
             return false;
     }
     return true;
 }
-template<typename Data>
-Data LinearContainer<Data>::Front() const
+bool LinearContainer<Data>::operator!=(const LinearContainer<Data> &lc) const noexcept
 {
-    if(this.size == 0)
-        throw std::out_of_range("Index out of bounds!");
-    return this[0];
+    return !(*this == lc);
 }
 template<typename Data>
-Data LinearContainer<Data>::Back() const
+Data& LinearContainer<Data>::Front() const
 {
     if(this.size == 0)
         throw std::out_of_range("Index out of bounds!");
-    return this[size-1];
+    return operator[](0);
+}
+template<typename Data>
+Data& LinearContainer<Data>::Back() const
+{
+    if(this.size == 0)
+        throw std::out_of_range("Index out of bounds!");
+    return operator[](size-1);
 }
 template <typename Data>
-bool TestableContainer<Data>::Exists(Data &d)
+
+//FoldableContainer functions
+template <typename Data>
+void AuxFoldExists(const Data& dat,const void* val,void* exists) noexcept
 {
-
+    if(dat == *((Data*) val))
+    {
+        *((bool*) exists) = true;
+    }
 }
-bool FoldableContainer<Data>::Exists(const Data d)
+template <typename Data>
+bool FoldableContainer<Data>::Exists(const Data& d) const noexcept
 {
-    //chiamata a FoldableContainer::Fold()
-    return true;
+    bool esiste;
+    Fold(&AuxFoldExists<Data>,&d,&esiste);
+    return esiste;
+}
+//PreOrderMappableContainer functions
+template <typename Data>
+void PreOrderMappableContainer<Data>::Map(MapFunctor fun, void* par)
+{
+    MapPreOrder(fun,par);
 }
 
-
-
-
+//PreOrderFoldableContainer functions
+template <typename Data>
+void PreOrderFoldableContainer<Data>::Fold(FoldFunctor fun,const void* par,void* acc) const
+{
+    FoldPreOrder(fun,par,acc);
+}
+//PostOrderMappableContainer functions
+template <typename Data>
+void PostOrderMappableContainer<Data>::Map(MapFunctor fun,void* par)
+{
+    MapPostOrder(fun,par);
+}
+//PostOrderFoldableContainer functions
+template <typename Data>
+void PostOrderFoldableContainer<Data>::Fold(FoldFunctor fun,const void* par,void* acc) const
+{
+    FoldInOrder(fun,par,acc);
+}
+//InOrderMappableContainer functions
+template <typename Data>
+void InOrderMappableContainer<Data>::Map(MapFunctor fun,void* par)
+{
+    MapInOrder(fun,par);
+}
+//InOrderFoldableContainer functions
+template <typename Data>
+void InOrderFoldableContainer<Data>::Fold(FoldFunctor fun, const void* par, void* acc) const
+{
+    FoldInOrder(fun,par,acc);
+}
+//BreadthMappableContainer functions
+template <typename Data>
+void BreadthMappableContainer<Data>::Map(MapFunctor fun,void* par)
+{
+    MapBreadth(fun,par);
+}
+//BreadthFoldableContainer functions
+template <typename Data>
+void BreadthFoldableContainer<Data>::Fold(FoldFunctor fun, const void* par, void* acc) const
+{
+    FoldBreadth(fun,par,acc);
+}
 /* ************************************************************************** */
 
 }
