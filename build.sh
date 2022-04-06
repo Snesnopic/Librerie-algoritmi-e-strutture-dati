eb985978504f7606582f5bef8df471e4fae9883f
Giuseppe <snesnopic@gmail.com>
2022-04-06 22:22:30 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 
 #! /bin/bash
 
-g++ -O3 -o main \
+g++ -O3 -ggdb3 -Weffc++ -o main \
   zlasdtest/exercise1/simpletest.cpp zlasdtest/exercise1/fulltest.cpp \
   zlasdtest/container/container.cpp \
   zlasdtest/test.cpp zmytest/test.cpp main.cpp
+
+valgrind --leak-check=full \
+         --show-leak-kinds=all \
+         --track-origins=yes \
+         --verbose \
+         --log-file=valgrind-out.txt \
+         ./main
