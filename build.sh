502c41c83ecff6804d754eabe3af434503fc0298
Giuseppe <snesnopic@gmail.com>
2022-04-07 19:07:55 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 
 #! /bin/bash
 
-g++ -O3 -ggdb3 -Weffc++ -o main \
+g++ -O3 -o main \
   zlasdtest/exercise1/simpletest.cpp zlasdtest/exercise1/fulltest.cpp \
   zlasdtest/container/container.cpp \
   zlasdtest/test.cpp zmytest/test.cpp main.cpp
