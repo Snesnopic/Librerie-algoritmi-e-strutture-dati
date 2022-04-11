0d709c76d740588270117c6def64feea18b8d419
Giuseppe <snesnopic@gmail.com>
2022-04-11 19:23:30 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-g++ -O3 -o main.exe zlasdtest\exercise1\simpletest.cpp zlasdtest\exercise1\fulltest.cpp zlasdtest\container\container.cpp zlasdtest\test.cpp zmytest\test.cpp main.cpp
+g++ -O3 -o main.exe zlasdtest\exercise1\simpletest.cpp zlasdtest\exercise1\fulltest.cpp zlasdtest/exercise2/simpletest.cpp zlasdtest/exercise2/fulltest.cpp zlasdtest\container\container.cpp zlasdtest\test.cpp zmytest\test.cpp main.cpp
 pause
