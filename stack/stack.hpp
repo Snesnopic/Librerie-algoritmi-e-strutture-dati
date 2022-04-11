b6909a83cdb976e9cdf32601181772d81ee91070
Giuseppe <snesnopic@gmail.com>
2022-04-11 19:23:42 +0200

    Update stack.hpp

--- stack/stack.hpp
+++ stack/stack.hpp
@@ -47,7 +47,7 @@ public:
 
   // Specific member functions
 
-  virtual void Top() = 0; // (constant version; concrete function must throw std::length_error when empty)
+  virtual const Data& Top() const = 0; // (constant version; concrete function must throw std::length_error when empty)
   virtual Data& Top() = 0; // (concrete function must throw std::length_error when empty)
   virtual void Pop() = 0; // (concrete function must throw std::length_error when empty)
   virtual Data& TopNPop() = 0; // (concrete function must throw std::length_error when empty)
