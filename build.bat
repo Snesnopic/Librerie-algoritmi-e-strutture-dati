5c60284c93544e454e8d4e96160dc0f25b8413b4
Giuseppe <gius.francione@studenti.unina.it>
2022-05-03 11:02:25 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-g++ -O3 -o main.exe zlasdtest\exercise1\simpletest.cpp zlasdtest\exercise1\fulltest.cpp zlasdtest/exercise2/simpletest.cpp zlasdtest/exercise2/fulltest.cpp zlasdtest/exercise3/simpletest.cpp zlasdtest/exercise3/fulltest.cpp zlasdtest\container\container.cpp zlasdtest\test.cpp zmytest\test.cpp main.cpp
+g++ -O3 -march=native -o main.exe zlasdtest\exercise1\simpletest.cpp zlasdtest\exercise1\fulltest.cpp zlasdtest/exercise2/simpletest.cpp zlasdtest/exercise2/fulltest.cpp zlasdtest/exercise3/simpletest.cpp zlasdtest/exercise3/fulltest.cpp zlasdtest\container\container.cpp zlasdtest\test.cpp zmytest\test.cpp main.cpp
 .\main.exe
 pause
