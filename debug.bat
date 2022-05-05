284227a030b9e4c9652551a187cea53716e0ddd9
Giuseppe <snesnopic@gmail.com>
2022-05-05 10:22:14 +0200

    Create debug.bat

---
+++ debug.bat
@@ -0,0 +1,3 @@
+g++ -O0 -ggdb -g3 -o debug.exe zlasdtest\exercise1\simpletest.cpp zlasdtest\exercise1\fulltest.cpp zlasdtest/exercise2/simpletest.cpp zlasdtest/exercise2/fulltest.cpp zlasdtest/exercise3/simpletest.cpp zlasdtest/exercise3/fulltest.cpp zlasdtest\container\container.cpp zlasdtest\test.cpp zmytest\test.cpp main.cpp
+.\debug.exe
+pause
