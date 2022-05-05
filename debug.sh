2e280c84e97709c58e5c50941a6ad41d3428cf43
Giuseppe <snesnopic@gmail.com>
2022-05-05 10:22:13 +0200

    Create debug

---
+++ debug.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+g++ -O0 -ggdb3 -g3 -o debug \
+  zlasdtest/exercise1/simpletest.cpp zlasdtest/exercise1/fulltest.cpp \
+  zlasdtest/exercise2/simpletest.cpp zlasdtest/exercise2/fulltest.cpp \
+  zlasdtest/exercise3/simpletest.cpp zlasdtest/exercise3/fulltest.cpp \
+  zlasdtest/container/container.cpp \
+  zlasdtest/test.cpp zmytest/test.cpp main.cpp
