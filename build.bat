55cf0646517853ebb5061501a5433c6978725fba
Giuseppe <snesnopic@gmail.com>
2022-05-05 10:20:53 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-g++ -O3 -march=native -o main.exe zlasdtest\exercise1\simpletest.cpp zlasdtest\exercise1\fulltest.cpp zlasdtest/exercise2/simpletest.cpp zlasdtest/exercise2/fulltest.cpp zlasdtest/exercise3/simpletest.cpp zlasdtest/exercise3/fulltest.cpp zlasdtest\container\container.cpp zlasdtest\test.cpp zmytest\test.cpp main.cpp
-.\main.exe
+g++ -Ofast -fno-signed-zeros -fno-trapping-math -march=native -o main.exe zlasdtest\exercise1\simpletest.cpp zlasdtest\exercise1\fulltest.cpp zlasdtest/exercise2/simpletest.cpp zlasdtest/exercise2/fulltest.cpp zlasdtest/exercise3/simpletest.cpp zlasdtest/exercise3/fulltest.cpp zlasdtest\container\container.cpp zlasdtest\test.cpp zmytest\test.cpp main.cpp
+.\debug.exe
 pause
