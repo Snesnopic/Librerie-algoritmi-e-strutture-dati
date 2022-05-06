7b2c6f39e2157beee27cfe66760cd2727ec65ebb
Giuseppe <snesnopic@gmail.com>
2022-05-06 21:08:46 +0200

    Update debug.bat

--- debug.bat
+++ debug.bat
@@ -1,3 +1,3 @@
-g++ -O0 -ggdb -g3 -o debug.exe zlasdtest\exercise1\simpletest.cpp zlasdtest\exercise1\fulltest.cpp zlasdtest/exercise2/simpletest.cpp zlasdtest/exercise2/fulltest.cpp zlasdtest/exercise3/simpletest.cpp zlasdtest/exercise3/fulltest.cpp zlasdtest\container\container.cpp zlasdtest\test.cpp zmytest\test.cpp main.cpp
+g++ -O0 -ggdb -std=c++17 -g3 -o debug.exe zlasdtest\exercise1\simpletest.cpp zlasdtest\exercise1\fulltest.cpp zlasdtest/exercise2/simpletest.cpp zlasdtest/exercise2/fulltest.cpp zlasdtest/exercise3/simpletest.cpp zlasdtest/exercise3/fulltest.cpp zlasdtest\container\container.cpp zlasdtest\test.cpp zmytest\test.cpp main.cpp
 .\debug.exe
 pause
