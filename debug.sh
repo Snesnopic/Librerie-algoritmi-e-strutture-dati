5db50f79f94563e075e472dfc3da8677d94bef3f
Giuseppe <snesnopic@gmail.com>
2022-05-06 21:08:42 +0200

    Update debug.sh

--- debug.sh
+++ debug.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-g++ -O0 -ggdb3 -g3 -o debug \
+g++ -O0 -ggdb3 -std=c++17 -g3 -o debug \
   zlasdtest/exercise1/simpletest.cpp zlasdtest/exercise1/fulltest.cpp \
   zlasdtest/exercise2/simpletest.cpp zlasdtest/exercise2/fulltest.cpp \
   zlasdtest/exercise3/simpletest.cpp zlasdtest/exercise3/fulltest.cpp \
