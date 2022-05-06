0a1027a6a4f559e10ded82195acf10a0038d097c
Giuseppe <snesnopic@gmail.com>
2022-05-06 17:05:57 +0200

    Update bst.hpp

--- bst/bst.hpp
+++ bst/bst.hpp
@@ -1,4 +1,3 @@
-
 #ifndef BST_HPP
 #define BST_HPP
 
@@ -8,117 +7,119 @@
 
 /* ************************************************************************** */
 
-namespace lasd {
+namespace lasd
+{
 
 /* ************************************************************************** */
 
-template <typename Data>
-class BST { // Must extend BinaryTreeLnk<Data>
+    template<typename Data>
+    class BST
+    { // Must extend BinaryTreeLnk<Data>
 
-private:
+    private:
 
-  // ...
+        // ...
 
-protected:
+    protected:
 
-  // using BinaryTreeLnk<Data>::???;
+        // using BinaryTreeLnk<Data>::???;
 
-  // ...
+        // ...
 
-public:
+    public:
 
-  // Default constructor
-  // BST() specifiers;
+        // Default constructor
+        // BST() specifiers;
 
-  /* ************************************************************************ */
+        /* ************************************************************************ */
 
-  // Specific constructors
-  // BST(argument) specifiers; // A bst obtained from a LinearContainer
+        // Specific constructors
+        // BST(argument) specifiers; // A bst obtained from a LinearContainer
 
-  /* ************************************************************************ */
+        /* ************************************************************************ */
 
-  // Copy constructor
-  // BST(argument) specifiers;
+        // Copy constructor
+        // BST(argument) specifiers;
 
-  // Move constructor
-  // BST(argument) specifiers;
+        // Move constructor
+        // BST(argument) specifiers;
 
-  /* ************************************************************************ */
+        /* ************************************************************************ */
 
-  // Destructor
-  // ~BST() specifiers;
+        // Destructor
+        // ~BST() specifiers;
 
-  /* ************************************************************************ */
+        /* ************************************************************************ */
 
-  // Copy assignment
-  // type operator=(argument) specifiers;
+        // Copy assignment
+        // type operator=(argument) specifiers;
 
-  // Move assignment
-  // type operator=(argument) specifiers;
+        // Move assignment
+        // type operator=(argument) specifiers;
 
-  /* ************************************************************************ */
+        /* ************************************************************************ */
 
-  // Comparison operators
-  // type operator==(argument) specifiers;
-  // type operator!=(argument) specifiers;
+        // Comparison operators
+        // type operator==(argument) specifiers;
+        // type operator!=(argument) specifiers;
 
-  /* ************************************************************************ */
+        /* ************************************************************************ */
 
-  // Specific member functions
+        // Specific member functions
 
-  // type Min(argument) specifiers; // (concrete function must throw std::length_error when empty)
-  // type MinNRemove(argument) specifiers; // (concrete function must throw std::length_error when empty)
-  // type RemoveMin(argument) specifiers; // (concrete function must throw std::length_error when empty)
+        // type Min(argument) specifiers; // (concrete function must throw std::length_error when empty)
+        // type MinNRemove(argument) specifiers; // (concrete function must throw std::length_error when empty)
+        // type RemoveMin(argument) specifiers; // (concrete function must throw std::length_error when empty)
 
-  // type Max(argument) specifiers; // (concrete function must throw std::length_error when empty)
-  // type MaxNRemove(argument) specifiers; // (concrete function must throw std::length_error when empty)
-  // type RemoveMax(argument) specifiers; // (concrete function must throw std::length_error when empty)
+        // type Max(argument) specifiers; // (concrete function must throw std::length_error when empty)
+        // type MaxNRemove(argument) specifiers; // (concrete function must throw std::length_error when empty)
+        // type RemoveMax(argument) specifiers; // (concrete function must throw std::length_error when empty)
 
-  // type Predecessor(argument) specifiers; // (concrete function must throw std::length_error when not found)
-  // type PredecessorNRemove(argument) specifiers; // (concrete function must throw std::length_error when not found)
-  // type RemovePredecessor(argument) specifiers; // (concrete function must throw std::length_error when not found)
+        // type Predecessor(argument) specifiers; // (concrete function must throw std::length_error when not found)
+        // type PredecessorNRemove(argument) specifiers; // (concrete function must throw std::length_error when not found)
+        // type RemovePredecessor(argument) specifiers; // (concrete function must throw std::length_error when not found)
 
-  // type Successor(argument) specifiers; // (concrete function must throw std::length_error when not found)
-  // type SuccessorNRemove(argument) specifiers; // (concrete function must throw std::length_error when not found)
-  // type RemoveSuccessor(argument) specifiers; // (concrete function must throw std::length_error when not found)
+        // type Successor(argument) specifiers; // (concrete function must throw std::length_error when not found)
+        // type SuccessorNRemove(argument) specifiers; // (concrete function must throw std::length_error when not found)
+        // type RemoveSuccessor(argument) specifiers; // (concrete function must throw std::length_error when not found)
 
-  /* ************************************************************************ */
+        /* ************************************************************************ */
 
-  // Specific member functions (inherited from DictionaryContainer)
+        // Specific member functions (inherited from DictionaryContainer)
 
-  // type Insert(argument) specifiers; // Override DictionaryContainer member (Copy of the value)
-  // type Insert(argument) specifiers; // Override DictionaryContainer member (Move of the value)
-  // type Remove(argument) specifiers; // Override DictionaryContainer member
+        // type Insert(argument) specifiers; // Override DictionaryContainer member (Copy of the value)
+        // type Insert(argument) specifiers; // Override DictionaryContainer member (Move of the value)
+        // type Remove(argument) specifiers; // Override DictionaryContainer member
 
-  /* ************************************************************************ */
+        /* ************************************************************************ */
 
-  // Specific member functions (inherited from TestableContainer)
+        // Specific member functions (inherited from TestableContainer)
 
-  // type Exists(argument) specifiers; // Override TestableContainer member
+        // type Exists(argument) specifiers; // Override TestableContainer member
 
-protected:
+    protected:
 
-  // Auxiliary member functions
+        // Auxiliary member functions
 
-  // type DataNDelete(argument) specifiers;
+        // type DataNDelete(argument) specifiers;
 
-  // type Detach(argument) specifiers;
+        // type Detach(argument) specifiers;
 
-  // type DetachMin(argument) specifiers;
-  // type DetachMax(argument) specifiers;
+        // type DetachMin(argument) specifiers;
+        // type DetachMax(argument) specifiers;
 
-  // type Skip2Left(argument) specifiers;
-  // type Skip2Left(argument) specifiers;
+        // type Skip2Left(argument) specifiers;
+        // type Skip2Left(argument) specifiers;
 
-  // type FindPointerToMin(argument) specifiers; // Both mutable & unmutable versions
-  // type FindPointerToMax(argument) specifiers; // Both mutable & unmutable versions
+        // type FindPointerToMin(argument) specifiers; // Both mutable & unmutable versions
+        // type FindPointerToMax(argument) specifiers; // Both mutable & unmutable versions
 
-  // type FindPointerTo(argument) specifiers; // Both mutable & unmutable versions
+        // type FindPointerTo(argument) specifiers; // Both mutable & unmutable versions
 
-  // type FindPointerToPredecessor(argument) specifiers; // Both mutable & unmutable versions
-  // type FindPointerToSuccessor(argument) specifiers; // Both mutable & unmutable versions
+        // type FindPointerToPredecessor(argument) specifiers; // Both mutable & unmutable versions
+        // type FindPointerToSuccessor(argument) specifiers; // Both mutable & unmutable versions
 
-};
+    };
 
 /* ************************************************************************** */
 
