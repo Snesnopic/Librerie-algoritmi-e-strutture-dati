396b690b35e98bda1b9be2cd2558ffe5f21b54c4
Giuseppe <snesnopic@gmail.com>
2022-05-27 14:36:37 +0200

    Update htopnadr.hpp

--- hashtable/opnadr/htopnadr.hpp
+++ hashtable/opnadr/htopnadr.hpp
@@ -27,19 +27,22 @@ namespace lasd
 		using HashTable<Data>::a;
 		using HashTable<Data>::b;
 		using HashTable<Data>::p;
-		using HashTable<Data>>::hash;
-		Vector<Data> table;
+		using HashTable<Data>::hash;
+		Vector<Data> table{};
 		// ...
 
 	public:
 
 		// Default constructor
-		// HashTableOpnAdr() specifiers;
+		HashTableOpnAdr() = default;
 
 		/* ************************************************************************ */
 
 		// Specific constructors
-		HashTableOpnAdr(unsigned long s); // A hash table of a given size
+		HashTableOpnAdr(unsigned long s) // A hash table of a given size
+		{
+			table.Resize(s);
+		}
 		HashTableOpnAdr(const LinearContainer<Data>& lc); // A hash table obtained from a LinearContainer
 		HashTableOpnAdr(unsigned long s, const LinearContainer<Data>& ls); // A hash table of a given size obtained from a LinearContainer
 
@@ -59,16 +62,16 @@ namespace lasd
 		/* ************************************************************************ */
 
 		// Copy assignment
-		// type operator=(argument) specifiers;
+		HashTableOpnAdr& operator=(const HashTableOpnAdr& ht);
 
 		// Move assignment
-		// type operator=(argument) specifiers;
+		HashTableOpnAdr& operator=(HashTableOpnAdr&& ht) noexcept;
 
 		/* ************************************************************************ */
 
 		// Comparison operators
-		virtual bool operator==(const HashTableOpnAdr& ht) const noexcept;
-		virtual bool operator!=(const HashTableOpnAdr& ht) const noexcept;
+		bool operator==(const HashTableOpnAdr& ht) const noexcept;
+		bool operator!=(const HashTableOpnAdr& ht) const noexcept;
 
 		/* ************************************************************************ */
 
@@ -85,42 +88,91 @@ namespace lasd
 
 		bool Insert(const Data& d) // Override DictionaryContainer member (Copy of the value)
 		{
-
+			if(table[HashKey(d)])
+			{
+				size++;
+				return true;
+			}
+			return false;
 		}
 		bool Insert(Data&& d) // Override DictionaryContainer member (Move of the value)
 		{
-
+			if(table[HashKey(d)])
+			{
+				size++;
+				return true;
+			}
+			return false;
 		}
 		bool Remove(const Data& d) // Override DictionaryContainer member
 		{
-
+			if(table[HashKey(d)])
+			{
+				size--;
+				return true;
+			}
+			return false;
 		}
+		void Insert(const LinearContainer<Data>& lc)
+		{
+			DictionaryContainer<Data>::Insert(lc);
+		};
+		void Insert(LinearContainer<Data>&& lc)
+		{
+			DictionaryContainer<Data>::Insert(std::move(lc));
+		};
+		void Remove(const LinearContainer<Data>& lc)
+		{
+			DictionaryContainer<Data>::Remove(lc);
+		};
 		/* ************************************************************************ */
 
 		// Specific member functions (inherited from TestableContainer)
 
 		bool Exists(const Data& d) // Override TestableContainer member
 		{
-
+			return table.Exists(d);
 		}
 
 		/* ************************************************************************ */
 
 		// Specific member functions (inherited from MappableContainer)
 
-		// type Map(argument) specifiers; // Override MappableContainer member
+		using typename MappableContainer<Data>::MapFunctor;
+
+		void Map(MapFunctor f, void *par) override // Override MappableContainer member
+		{/*
+			for(unsigned long i = 0 ; i < table.Size() ; i++)
+			{
+				if(!table[i].Empty)
+					table[i].Map(f,par);
+			}*/
+		}
 
 		/* ************************************************************************ */
 
 		// Specific member functions (inherited from FoldableContainer)
 
-		// type Fold(argument) specifiers; // Override FoldableContainer member
+		using typename FoldableContainer<Data>::FoldFunctor;
+
+		void Fold(FoldFunctor f, const void *par, void *acc) const override // Override FoldableContainer member
+		{/*
+			for(unsigned long i = 0 ; i < table.Size() ; i++)
+			{
+				if(!table[i].Empty)
+					table[i].Fold(f,par,acc);
+			}*/
+		}
 
 		/* ************************************************************************ */
 
 		// Specific member functions (inherited from Container)
 
-		// type Clear() specifiers; // Override Container member
+		void Clear() noexcept // Override Container member
+		{
+			table.Clear();
+			size = 0;
+		}
 
 	public:
 
