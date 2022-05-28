1e17b11785244fb6267e7c767a31ab9c48237150
Giuseppe <snesnopic@gmail.com>
2022-05-28 22:25:54 +0200

    Update htclsadr

--- hashtable/clsadr/htclsadr.cpp
+++ hashtable/clsadr/htclsadr.cpp
@@ -2,7 +2,210 @@ namespace lasd
 {
 
 /* ************************************************************************** */
-	
+	template<typename Data>
+	unsigned long HashTableClsAdr<Data>::HashKey(const Data& d) const // Hash function
+	{
+		return (((a * hash(d)) + b) % p) % table.Size();
+	}
+
+	template<typename Data>
+	HashTableClsAdr<Data>::HashTableClsAdr(unsigned long s)
+	{
+		table.Resize(s);
+	}
+	template<typename Data>
+	HashTableClsAdr<Data>::HashTableClsAdr(unsigned long s, const LinearContainer<Data>& lc) // A hash table of a given size obtained from a LinearContainer
+	{
+		table.Resize(s);
+		Insert(lc);
+	}
+
+	// Copy constructor
+	template<typename Data>
+	HashTableClsAdr<Data>::HashTableClsAdr(const HashTableClsAdr& ht)
+	{
+		HashTable<Data>::operator=(ht);
+		size = ht.size;
+		table = ht.table;
+	}
+
+	// Move constructor
+	template<typename Data>
+	HashTableClsAdr<Data>::HashTableClsAdr(HashTableClsAdr&& ht) noexcept
+	{
+		HashTable<Data>::operator=(std::move(ht));
+		table = std::move(ht.table);
+	}
+
+	// Destructor
+	template<typename Data>
+	HashTableClsAdr<Data>::~HashTableClsAdr()
+	{
+		table.Clear();
+	}
+
+	// Copy assignment
+	template<typename Data>
+	HashTableClsAdr<Data>& HashTableClsAdr<Data>::operator=(const HashTableClsAdr& ht)
+	{
+		if (*this != ht)
+		{
+			HashTable<Data>::operator=(ht);
+			size = ht.size;
+			table = ht.table;
+		}
+		return *this;
+	}
+
+	// Move assignment
+	template<typename Data>
+	HashTableClsAdr<Data>& HashTableClsAdr<Data>::operator=(HashTableClsAdr&& ht) noexcept
+	{
+		if (*this != ht)
+		{
+			HashTable<Data>::operator=(std::move(ht));
+			table = std::move(ht.table);
+		}
+		return *this;
+	}
+
+	// Comparison operators
+	template<typename Data>
+	bool HashTableClsAdr<Data>::operator==(const HashTableClsAdr& ht) const noexcept
+	{
+		if (size == ht.size)
+		{
+			BST<Data> bst1;
+			for (unsigned long i = 0; i < table.Size(); i++)
+			{
+				BTInOrderIterator<Data> j(table[i]);
+				while (!j.Terminated())
+				{
+					bst1.Insert(*j);
+					++j;
+				}
+			}
+			BST<Data> bst2;
+			for (unsigned long i = 0; i < ht.table.Size(); i++)
+			{
+				BTInOrderIterator<Data> j(ht.table[i]);
+				while (!j.Terminated())
+				{
+					bst2.Insert(*j);
+					++j;
+				}
+			}
+			return bst1 == bst2;
+		}
+		return false;
+	}
+
+	template<typename Data>
+	bool HashTableClsAdr<Data>::operator!=(const HashTableClsAdr& ht) const noexcept
+	{
+		return !(*this == ht);
+	}
+
+	// Specific member functions (inherited from HashTable)
+	template<typename Data>
+	void HashTableClsAdr<Data>::Resize(const unsigned long newSize) // Resize the hashtable to a given size
+	{
+		HashTableClsAdr<Data> newHash(newSize);
+		for (unsigned long i = 0; i < table.Size(); i++)
+		{
+			if (table[i].Empty())
+				continue;
+			else
+			{
+				BTBreadthIterator<Data> j(table[i]);
+				while (!j.Terminated())
+				{
+					newHash.Insert(*j);
+					++j;
+				}
+				table[i].Clear();
+			}
+		}
+		table.Clear();
+		std::swap(*this, newHash);
+	}
+
+	// Specific member functions (inherited from DictionaryContainer)
+	template<typename Data>
+	bool HashTableClsAdr<Data>::Insert(const Data& d) // Override DictionaryContainer member (Copy of the value)
+	{
+		unsigned long j = HashKey(d);
+		if (table[j].Insert(d))
+		{
+			size++;
+			return true;
+		}
+		return false;
+	}
+
+	template<typename Data>
+	bool HashTableClsAdr<Data>::Insert(Data&& d) noexcept // Override DictionaryContainer member (Move of the value)
+	{
+		unsigned long j = HashKey(d);
+		if (table[j].Insert(std::move(d)))
+		{
+			size++;
+			return true;
+		}
+		return false;
+	}
+
+	template<typename Data>
+	bool HashTableClsAdr<Data>::Remove(const Data& d) // Override DictionaryContainer member
+	{
+		unsigned long j = HashKey(d);
+		if (table[j].Remove(d))
+		{
+			size--;
+			return true;
+		}
+		return false;
+	}
+
+	// Specific member functions (inherited from TestableContainer)
+	template<typename Data>
+	bool HashTableClsAdr<Data>::Exists(const Data& d) const noexcept // Override TestableContainer member
+	{
+		unsigned long j = HashKey(d);
+		return table[j].Exists(d);
+	}
+
+	// Specific member functions (inherited from MappableContainer)
+	template<typename Data>
+	void HashTableClsAdr<Data>::Map(MapFunctor f, void *par) // Override MappableContainer member
+	{
+		for (unsigned long i = 0; i < table.Size(); i++)
+		{
+			if (!table[i].Empty())
+				table[i].Map(f, par);
+		}
+	}
+
+	// Specific member functions (inherited from FoldableContainer)
+	template<typename Data>
+	void HashTableClsAdr<Data>::Fold(FoldFunctor f, const void *par, void *acc) const // Override FoldableContainer member
+	{
+		for (unsigned long i = 0; i < table.Size(); i++)
+		{
+			if (!table[i].Empty())
+				table[i].Fold(f, par, acc);
+		}
+	}
+
+	// Specific member functions (inherited from Container)
+	template<typename Data>
+	void HashTableClsAdr<Data>::Clear() noexcept // Override Container member
+	{
+		table.Clear();
+		table.Resize(127);
+		size = 0;
+	}
+
 /* ************************************************************************** */
 
 }
--- hashtable/clsadr/htclsadr.hpp
+++ hashtable/clsadr/htclsadr.hpp
@@ -30,12 +30,11 @@ namespace lasd
 		using HashTable<Data>::p;
 		using HashTable<Data>::hash;
 		using HashTable<Data>::HashKey;
-		Vector<List<Data>> table{};
+		using DictionaryContainer<Data>::Insert;
+		using DictionaryContainer<Data>::Remove;
+		Vector<BST<Data>> table{};
 		// ...
-		unsigned long HashKey(const Data& d) const override
-		{
-			return (((a*hash(d))+b)% p) % table.Size();
-		}
+		unsigned long HashKey(const Data& d) const override;
 	public:
 
 		// Default constructor
@@ -44,176 +43,61 @@ namespace lasd
 		/* ************************************************************************ */
 
 		// Specific constructors
-		HashTableClsAdr(unsigned long s) // A hash table of a given size
-		{
-			table.Resize(s);
-		}
+		HashTableClsAdr(unsigned long s); // A hash table of a given size
+
 		HashTableClsAdr(const LinearContainer<Data>& lc) : HashTableClsAdr()// A hash table obtained from a LinearContainer
 		{
 			Insert(lc);
 		}
-		HashTableClsAdr(unsigned long s, const LinearContainer<Data>& lc) // A hash table of a given size obtained from a LinearContainer
-		{
-            table.Resize(s);
-			Insert(lc);
-		}
+		HashTableClsAdr(unsigned long s, const LinearContainer<Data>& lc); // A hash table of a given size obtained from a LinearContainer
+
 
 		/* ************************************************************************ */
 
 		// Copy constructor
-		HashTableClsAdr(const HashTableClsAdr& ht)
-		{
-			HashTable<Data>::operator=(ht);
-            size = ht.size;
-            table = ht.table;
-		}
+		HashTableClsAdr(const HashTableClsAdr& ht);
 
 		// Move constructor
-		HashTableClsAdr(HashTableClsAdr&& ht) noexcept
-		{
-			HashTable<Data>::operator=(std::move(ht));
-			table = std::move(ht.table);
-		}
+		HashTableClsAdr(HashTableClsAdr&& ht) noexcept;
 
 		/* ************************************************************************ */
 
 		// Destructor
-		virtual ~HashTableClsAdr()
-		{
-			table.Clear();
-		}
+		virtual ~HashTableClsAdr();
 
 		/* ************************************************************************ */
 
 		// Copy assignment
-		HashTableClsAdr& operator=(const HashTableClsAdr& ht)
-		{
-			if(*this != ht)
-			{
-				HashTable<Data>::operator=(ht);
-                size = ht.size;
-				table = ht.table;
-			}
-			return *this;
-		}
+		HashTableClsAdr& operator=(const HashTableClsAdr& ht);
 
 		// Move assignment
-		HashTableClsAdr& operator=(HashTableClsAdr&& ht) noexcept
-		{
-			if(*this != ht)
-			{
-				HashTable<Data>::operator=(std::move(ht));
-				table = std::move(ht.table);
-			}
-			return *this;
-		}
+		HashTableClsAdr& operator=(HashTableClsAdr&& ht) noexcept;
 
 		/* ************************************************************************ */
 
 		// Comparison operators
-		bool operator==(const HashTableClsAdr& ht) const noexcept
-		{
-			if(size == ht.size)
-			{
-				BST<Data> v1;
-                BST<Data> v2;
-                for(unsigned long i = 0;i <table.Size();i++)
-                {
-                    for(unsigned long j = 0; j<table[i].Size() ;j++)
-                    {
-                        v1.Insert(table[i][j]);
-                    }
-                }
-                for(unsigned long i = 0;i <ht.table.Size();i++)
-                {
-                    for(unsigned long j = 0; j<ht.table[i].Size() ;j++)
-                    {
-                        v2.Insert(ht.table[i][j]);
-                    }
-                }
-                return v1 == v2;
-            }
-			return false;
-		}
-		bool operator!=(const HashTableClsAdr& ht) const noexcept
-		{
-			return !(*this == ht);
-		}
+		bool operator==(const HashTableClsAdr& ht) const noexcept;
+		bool operator!=(const HashTableClsAdr& ht) const noexcept;
 
 		/* ************************************************************************ */
 
 		// Specific member functions (inherited from HashTable)
 
-		void Resize(const unsigned long newSize) // Resize the hashtable to a given size
-		{
-            HashTableClsAdr<Data> newHash(newSize);
-            for(unsigned long i = 0;i <table.Size();i++)
-            {
-                for(unsigned long j = 0; j<table[i].Size() ;j++)
-                {
-                    newHash.Insert(table[i][j]);
-                }
-            }
-            table.Clear();
-            std::swap(*this,newHash);
-
-		}
+		void Resize(const unsigned long newSize); // Resize the hashtable to a given size
 
 		/* ************************************************************************ */
 
 		// Specific member functions (inherited from DictionaryContainer)
 
-		bool Insert(const Data& d) // Override DictionaryContainer member (Copy of the value)
-		{
-            unsigned long j = HashKey(d);
-            if(table[j].Insert(d))
-            {
-                size++;
-                return true;
-            }
-            return false;
-		}
-		bool Insert(Data&& d) noexcept// Override DictionaryContainer member (Move of the value)
-		{
-            unsigned long j = HashKey(d);
-            if(table[j].Insert(std::move(d)))
-			{
-				size++;
-				return true;
-			}
-			return false;
-		}
-		bool Remove(const Data& d) // Override DictionaryContainer member
-		{
-			unsigned long j = HashKey(d);
-			if(table[j].Remove(d))
-			{
-				size--;
-				return true;
-			}
-			return false;
-		}
-		void Insert(const LinearContainer<Data>& lc)
-		{
-			DictionaryContainer<Data>::Insert(lc);
-		};
-		void Insert(LinearContainer<Data>&& lc) noexcept
-		{
-			DictionaryContainer<Data>::Insert(std::move(lc));
-		};
-		void Remove(const LinearContainer<Data>& lc)
-		{
-			DictionaryContainer<Data>::Remove(lc);
-		};
+		bool Insert(const Data& d); // Override DictionaryContainer member (Copy of the value)
+		bool Insert(Data&& d) noexcept; // Override DictionaryContainer member (Move of the value)
+		bool Remove(const Data& d); // Override DictionaryContainer member
+
 		/* ************************************************************************ */
 
 		// Specific member functions (inherited from TestableContainer)
 
-		bool Exists(const Data& d) const noexcept // Override TestableContainer member
-		{
-			unsigned long j = HashKey(d);
-            return table[j].Exists(d);
-		}
+		bool Exists(const Data& d) const noexcept; // Override TestableContainer member
 
 		/* ************************************************************************ */
 
@@ -221,14 +105,8 @@ namespace lasd
 
 		using typename MappableContainer<Data>::MapFunctor;
 
-		void Map(MapFunctor f, void *par) override // Override MappableContainer member
-		{
-			for(unsigned long i = 0 ; i < table.Size() ; i++)
-			{
-				if(!table[i].Empty())
-					table[i].Map(f,par);
-			}
-		}
+		void Map(MapFunctor f, void *par) override; // Override MappableContainer member
+
 
 		/* ************************************************************************ */
 
@@ -236,25 +114,13 @@ namespace lasd
 
 		using typename FoldableContainer<Data>::FoldFunctor;
 
-		void Fold(FoldFunctor f, const void *par, void *acc) const override // Override FoldableContainer member
-		{
-			for(unsigned long i = 0 ; i < table.Size() ; i++)
-			{
-				if(!table[i].Empty())
-					table[i].Fold(f,par,acc);
-			}
-		}
+		void Fold(FoldFunctor f, const void *par, void *acc) const override; // Override FoldableContainer member
 
 		/* ************************************************************************ */
 
 		// Specific member functions (inherited from Container)
 
-		void Clear() noexcept // Override Container member
-		{
-			table.Clear();
-            table.Resize(127);
-			size = 0;
-		}
+		void Clear() noexcept; // Override Container member
 
 	};
 
