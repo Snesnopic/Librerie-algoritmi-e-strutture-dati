a176b86f027e19d494ce37f7d17f246257a0142e
Giuseppe <snesnopic@gmail.com>
2022-08-03 20:01:37 +0200

    Rimossa compilazione fulltest

--- debug.bat
+++ debug.bat
@@ -1,3 +1,3 @@
-g++ -O0 -ggdb3 -std=c++17 -g3 -o debug.exe zlasdtest\exercise1\simpletest.cpp zlasdtest\exercise1\fulltest.cpp zlasdtest\exercise2\simpletest.cpp zlasdtest\exercise2\fulltest.cpp zlasdtest\exercise3\simpletest.cpp zlasdtest\exercise3\fulltest.cpp zlasdtest\exercise4\simpletest.cpp zlasdtest\exercise4\fulltest.cpp zlasdtest\exercise5\simpletest.cpp zlasdtest\exercise5\fulltest.cpp zlasdtest\container\container.cpp zlasdtest\test.cpp zmytest\test.cpp main.cpp
+g++ -O0 -ggdb3 -std=c++17 -g3 -o debug.exe zlasdtest\exercise1\simpletest.cpp zlasdtest\exercise2\simpletest.cpp zlasdtest\exercise3\simpletest.cpp zlasdtest\exercise4\simpletest.cpp zlasdtest\exercise5\simpletest.cpp zlasdtest\container\container.cpp zlasdtest\test.cpp zmytest\test.cpp main.cpp
 .\debug.exe
 pause
