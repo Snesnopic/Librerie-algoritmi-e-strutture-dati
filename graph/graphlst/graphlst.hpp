6a441b7be05645e3e86a59916cd116ec1422bd42
Giuseppe <snesnopic@gmail.com>
2022-11-30 10:04:01 +0100

    Aggiunto KeyGen per la Bimap

--- graph/graphlst/graphlst.hpp
+++ graph/graphlst/graphlst.hpp
@@ -2,8 +2,9 @@
 #define GRAPHLST_HPP
 
 #include "../graph.hpp"
-#include "../../vector/vector.hpp"
 #include "../../list/list.hpp"
+#include "../../vector/vector.hpp"
+#include "../../bimap/bimap.hpp"
 
 namespace lasd
 {
@@ -13,9 +14,34 @@ namespace lasd
 	template<typename Data>
 	class GraphLst : virtual public Graph<Data>
 	{
+		private:
+		struct KeyGen
+		{
+			unsigned long key;
+			KeyGen() : key(0){};
+			unsigned long operator()(unsigned long x = 1)
+			{
+				return (key += x);
+			}
+
+		};
+		struct Edge
+		{
+			long destination{};
+			double weight{};
+			bool operator==(const Edge& it) const{
+				return (true);
+			}
+
+			bool operator!=(const Edge& it) const{
+				return !(*this == it);
+			}
+		};
 	protected:
-		Vector<List<Data>> adjList;
-		List<Data> indices;
+		KeyGen kg;
+		friend class Bimap<Data,long>;
+		Bimap<Data,long> nameMap;
+		Vector<Vector<Edge>> graph;
 		using Graph<Data>::size;
 	public:
 		GraphLst() = default;
@@ -24,14 +50,10 @@ namespace lasd
 
 		explicit GraphLst(const Vector<Data>& vertices);
 
-		explicit GraphLst(unsigned long expectedSize);
-
 		GraphLst(const GraphLst<Data>& gm);
 
 		GraphLst(GraphLst<Data>&& gm) noexcept;
 
-
-	public:
 		[[nodiscard]] unsigned long Vertices() const noexcept override;
 
 		[[nodiscard]] unsigned long Edges() const noexcept override;
@@ -42,7 +64,7 @@ namespace lasd
 
 		void RemoveEdge(Data& v1, Data& v2) override;
 
-		void AddVertex(Data& v1) override;
+		void AddVertex(const Data& v1) override;
 
 		void AddVertex(Data&& v1) noexcept override;
 
@@ -54,10 +76,10 @@ namespace lasd
 
 		void AddEdge(const Data& v1, const Data& v2, bool isDirected) override;
 
-		bool AreLinked(const Data& v1, const Data& v2) const override;
+		//bool AreLinked(const Data& v1, const Data& v2) const override;
 
 		bool AreDirectlyLinked(const Data& v1, const Data& v2) const override;
-
+		/*
 		double WalkWeight(const Data& v1, const Data& v2) const override;
 
 		[[nodiscard]] bool IsAcyclic() const override;
@@ -74,28 +96,32 @@ namespace lasd
 
 		GraphLst<Data> Reverse() const override;
 
+*/		bool operator==(const GraphLst<Data>& g) const;
+		bool operator!=(const GraphLst<Data>& g) const;
+
+		void Clear() override;
 		Vector<Data> LinksOf(Data& v1) const override;
 
 		// Specific member functions (inherited from MappableContainer)
-
+/*/
 		using typename MappableContainer<Data>::MapFunctor;
 
 		void Map(MapFunctor f, void *par) override; // Override MappableContainer member
 		void MapBreadth(MapFunctor f, void *par) override;
 
 		void MapDepth(MapFunctor f, void *par) override;
-
+*/
 		/* ************************************************************************ */
 
 		// Specific member functions (inherited from FoldableContainer)
-
+/*/
 		using typename FoldableContainer<Data>::FoldFunctor;
 
 		void Fold(FoldFunctor f, const void *par, void *acc) const override; // Override FoldableContainer member
 		void FoldBreadth(FoldFunctor f, const void *par, void *acc) const override;
 
 		void FoldDepth(FoldFunctor f, const void *par, void *acc) const override;
-
+*/
 	};
 }
 
