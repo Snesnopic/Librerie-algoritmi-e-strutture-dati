4dff85054dea644f5c30a3f048619a1e8e17b3ef
Giuseppe <snesnopic@gmail.com>
2023-05-14 09:55:27 +0200

    Commit iniziale ValMatrix

---
+++ matrix/valmatrix/valmatrix.cpp
@@ -0,0 +1,176 @@
+namespace lasd
+{
+    // Destructor
+    template<typename Data>
+    ValMatrix<Data>::~ValMatrix()
+    {
+        for (unsigned long i = 0; i < matrix.Size(); ++i)
+        {
+            matrix[i].Clear();
+        }
+        matrix.Clear();
+    }
+
+    /* ************************************************************************ */
+
+    // Copy assignment
+    template<typename Data>
+    ValMatrix<Data>& ValMatrix<Data>::operator=(const ValMatrix& m)
+    {
+        if (this != &m)
+        {
+            matrix = m.matrix;
+            size = m.size;
+            rows = m.rows;
+            cols = m.cols;
+        }
+        return *this;
+    }
+
+    // Move assignment
+    template<typename Data>
+    ValMatrix<Data>& ValMatrix<Data>::operator=(ValMatrix&& m) noexcept
+    {
+        if (this != &m)
+        {
+            matrix = std::move(m.matrix);
+            size = std::move(m.size);
+            rows = std::move(m.rows);
+            cols = std::move(m.cols);
+        }
+        return *this;
+    }
+
+    /* ************************************************************************ */
+
+    // Comparison operators
+    template<typename Data>
+    bool ValMatrix<Data>::operator==(const ValMatrix& m) const noexcept
+    {
+        if (size == m.size && cols == m.cols && rows == m.rows)
+        {
+            for (unsigned long i = 0; i < rows; ++i)
+            {
+                for (unsigned long j = 0; j < cols; ++j)
+                {
+                    if (matrix[i][j] != m.matrix[i][j])
+                        return false;
+                }
+            }
+        }
+        else
+            return false;
+        return true;
+    }
+
+    template<typename Data>
+    bool ValMatrix<Data>::operator!=(const ValMatrix& m) const noexcept
+    {
+        return !(*this == m);
+    }
+
+    template<typename Data>
+    ValMatrix<Data> ValMatrix<Data>::Transpose()
+    {
+        ValMatrix<Data> newMatrix(cols,rows);
+        for(int i = 0; i < rows; ++i)
+        {
+            for(int j = 0; j < cols; ++j)
+            {
+                newMatrix[j][i] = matrix[i][j];
+            }
+        }
+        return newMatrix;
+    }
+
+    template<typename Data>
+    ValMatrix<Data>& ValMatrix<Data>::operator+(const ValMatrix<Data>& other)
+    {
+        if(rows == other.rows && cols == other.cols)
+        {
+            ValMatrix<Data> newMatrix(rows,cols);
+            for(int i = 0; i < rows; ++i)
+            {
+                for(int j = 0; j < cols; ++j)
+                {
+                    newMatrix[i][j] = matrix[i][j] + other.matrix[i][j];
+                }
+            }
+            return newMatrix;
+        }
+        else
+            throw std::length_error("Tried to add two matrices with different sizes!");
+    }
+
+    template<typename Data>
+    ValMatrix<Data>& ValMatrix<Data>::operator*(const ValMatrix<Data>& other)
+    {
+
+    }
+
+    template<typename Data>
+    ValMatrix<Data>& ValMatrix<Data>::operator*(const auto& other)
+    {
+        if(rows == other.rows && cols == other.cols)
+        {
+            ValMatrix<Data> newMatrix(rows,cols);
+            for(int i = 0; i < rows; ++i)
+            {
+                for(int j = 0; j < cols; ++j)
+                {
+                    newMatrix[i][j] = matrix[i][j] * other;
+                }
+
+            }
+            return newMatrix;
+        }
+    }
+    template<typename Data>
+    ValMatrix<Data> ValMatrix<Data>::SubMatrixRemoving(unsigned long rowToExclude,unsigned long colToExclude)
+    {
+        ValMatrix<Data> subMatrix;
+        for(int i = 0; i < rows; i++)
+        {
+            if(i != rowToExclude)
+            {
+                Vector<Data> rowVec;
+                for(int j = 0; j < cols; j++)
+                {
+                    if(j != colToExclude)
+                    {
+                        rowVec.Insert(matrix[i][j]);
+                    }
+                }
+                subMatrix.matrix.Insert(rowVec);
+            }
+        }
+        return subMatrix;
+    }
+    template<typename Data>
+    Data ValMatrix<Data>::Determinant()
+    {
+        if(rows != cols)
+            throw std::length_error("");
+        if(rows == 1)
+            return matrix[0][0];
+        if(rows == 2)
+            return (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0]);
+
+        Data det{};
+        int sign = 1;
+        for(int i = 0; i < rows; ++i)
+        {
+            auto subMatrix = SubMatrixRemoving(0,i);
+            det += sign * matrix[0][i] * subMatrix.Determinant();
+            sign *= -1;
+        }
+
+        return det;
+    }
+
+    template<typename Data>
+    ValMatrix<Data>& ValMatrix<Data>::Inverse()
+    {
+
+    }
+}
---
+++ matrix/valmatrix/valmatrix.hpp
@@ -0,0 +1,64 @@
+#ifndef LASD_VALMATRIX_HPP
+#define LASD_VALMATRIX_HPP
+
+#include "matrix.hpp"
+#include <valarray>
+
+namespace lasd
+{
+    template<typename Data>
+    class ValMatrix: virtual public Matrix<Data>
+    {
+    private:
+        ValMatrix<Data> SubMatrixRemoving(unsigned long rowToExclude,unsigned long colToExclude);
+    protected:
+        using Matrix<Data>::rows;
+        using Matrix<Data>::cols;
+        using Matrix<Data>::size;
+        using Matrix<Data>::matrix;
+    public:
+        using Matrix<Data>::Matrix;
+
+        // Copy constructor
+        ValMatrix(const ValMatrix& m) : Matrix<Data>(m) {}
+
+        // Move constructor
+        ValMatrix(ValMatrix&& m) noexcept : Matrix<Data>(m) {}
+
+        /* ************************************************************************ */
+
+        // Destructor
+        ~ValMatrix() override;
+
+        /* ************************************************************************ */
+
+        // Copy assignment
+        ValMatrix& operator=(const ValMatrix& m);
+
+        // Move assignment
+        ValMatrix& operator=(ValMatrix&& m) noexcept;
+
+        /* ************************************************************************ */
+
+        // Comparison operators
+        bool operator==(const ValMatrix& m) const noexcept;
+
+        bool operator!=(const ValMatrix& m) const noexcept;
+
+        /* ************************************************************************ */
+        ValMatrix<Data> Transpose();
+        ValMatrix<Data>& operator+(const ValMatrix<Data>& other);
+        ValMatrix<Data>& operator*(const ValMatrix<Data>& other);
+        ValMatrix<Data>& operator*(const auto& other);
+        Data Determinant();
+        ValMatrix<Data>& Inverse();
+    };
+
+
+
+}
+
+
+
+#include "valmatrix.cpp"
+#endif //LASD_VALMATRIX_HPP
